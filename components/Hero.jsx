e4e2f84d03b9a3c5c27dd68ab108ff8a637f5617
Joshuatauro <joshuatauro45@gmail.com>
2021-06-18 19:54:31 +0400

    Changed about me

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
             Hey there, I am
         </h2>
         <h1 className=" text-4xl md:text-6xl text-black font-black dark:text-white">Joshua Tauro</h1>
-        <p className="mt-2 text-gray-700 dark:text-gray-400 ">I am a <span className="text-blue-700 dark:text-blue-400">Fullstack Developer</span> who loves to convert dreams into reality. I am currently in college, but still open to new and exciting opportunities 🔥</p>
+        <p className="mt-2 text-gray-700 dark:text-gray-400 ">I am a 17 year old <span className="text-blue-700 dark:text-blue-400">Fullstack Developer</span> who loves to convert dreams into reality. I am currently in high school, but still open to new and exciting opportunities 🔥</p>
       </div>
     </section>
   )
